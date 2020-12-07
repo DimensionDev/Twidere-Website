68d3b1f19b0e8110ead9b015c22295090b0fd17f
Hancheng Zhou <z308114274@gmail.com>
2020-12-07 16:46:29 +0800

    add tailwindcss

---
+++ src/HomePage.tsx
@@ -0,0 +1,8 @@
+function HomePage() {
+  return (
+    <div className="HomePage">
+    </div>
+  );
+}
+
+export default HomePage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './main.css';
+import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
