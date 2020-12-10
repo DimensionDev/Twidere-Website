4ad39125313aaebf432b55c273c6aac8658bb0c7
Hancheng Zhou <z308114274@gmail.com>
2020-12-10 21:53:50 +0800

    done

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
     makeStyles,
     createStyles,
@@ -31,9 +31,10 @@ import {
     SujitechIcon,
     ArrowDropDownIcon,
 } from './assets/iconSvg'
-import { ImgSvg_1, ImgSvg_2, ImgSvg_3 } from './assets/imgSvg'
+import { ImgFeatureOne, ImgFeatureTwo, ImgFeatureThree, ImgFeatureFour } from './assets/imgSvg'
 import classNames from 'classnames'
 import { withTranslation, TFunction } from 'react-i18next'
+import i18n from './i18n'
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -79,11 +80,11 @@ const useStyles = makeStyles((theme) =>
             padding: '3rem',
         },
         drawer: {
-            width: 240,
+            width: 270,
             flexShrink: 0,
         },
         drawerPaper: {
-            width: 240,
+            width: 270,
         },
         drawerHeader: {
             display: 'flex',
@@ -179,6 +180,15 @@ const useStyles = makeStyles((theme) =>
             maxWidth: '90%',
             marginBottom: theme.spacing(8),
         },
+        featureWrapper: {
+            width: '85%',
+            marginBottom: '5rem',
+            [theme.breakpoints.down('md')]: {
+                width: '95%',
+                marginBottom: '2rem',
+            },
+            maxWidth: 1342,
+        },
         futureWrapper: {
             display: 'flex',
             justifyContent: 'space-between',
@@ -244,6 +254,7 @@ const useStyles = makeStyles((theme) =>
             [theme.breakpoints.down('md')]: {
                 width: '90%',
             },
+            maxWidth: 1142,
         },
         arrowDropDownIcon: {
             fontSize: '28px !important',
@@ -258,6 +269,12 @@ function HomePage({ t }: { t: TFunction }) {
     const [anchorEl, setAnchorEl] = useState<(EventTarget & HTMLButtonElement) | null>(null)
     const screenWidth = window.innerWidth
     const classes = useStyles({ screenWidth })
+
+    useEffect(() => {
+        const lang = window.localStorage.getItem('twidere_language')
+        console.log(lang, 'twidere_language')
+        setTimeout(() => i18n.changeLanguage(lang ? lang : 'zh'), 0)
+    }, [])
 
     return (
         <Box className={'flex flex-col items-center ' + classes.root}>
@@ -281,21 +298,15 @@ function HomePage({ t }: { t: TFunction }) {
                 </section>
                 <section className={`flex items-center justify-between w-96 ${xsMatched ? 'flex-row-reverse' : ''}`}>
                     {xsMatched ? (
-                        <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            edge="end"
-                            onClick={() => setOpen(!open)}
-                            //    className={clsx(open && classes.hide)}
-                        >
+                        <IconButton color="inherit" aria-label="open drawer" edge="end" onClick={() => setOpen(!open)}>
                             <MenuIcon fontSize="large" />
                         </IconButton>
                     ) : (
                         <>
-                            <Typography className="cursor-pointer text-black-">HOME</Typography>
-                            <Typography className="cursor-pointer">BLOG</Typography>
+                            <Typography className="cursor-pointer text-black-">{t('link_home')}</Typography>
+                            <Typography className="cursor-pointer">{t('link_blog')}</Typography>
                             <Button color="primary" className={classes.button} variant="outlined">
-                                <span className="font-bold">Get Twidere X</span>
+                                <span className="font-bold">{t('link_get_twidere')}</span>
                             </Button>
                         </>
                     )}
@@ -328,7 +339,7 @@ function HomePage({ t }: { t: TFunction }) {
                     </ListItem>
 
                     <Button color="primary" className={classes.button} variant="outlined">
-                        <span className="font-bold">Get Twidere X</span>
+                        <span className="font-bold">{t('link_get_twidere')}</span>
                     </Button>
                 </List>
             </Drawer>
@@ -339,18 +350,18 @@ function HomePage({ t }: { t: TFunction }) {
                     </Typography>
                     <div className="mb-2" />
                     <Typography className={classes.bannerIntro} variant="h6">
-                        没有⼴告，不收费⽤，⽀持开源，兼具丰富功能和⾃定设计
+                        {t('banner_intro')}
                     </Typography>
                 </main>
             ) : (
                 <main className={classes.main}>
                     <section className={classes.banner}>
                         <Typography className={classes.bannerTitle} variant="h3" component="h3">
-                            可⾃定义且开源的Twitter 客户端
+                            {t('banner_title')}
                         </Typography>
                         <div className="mb-2" />
                         <Typography className={classes.bannerIntro} variant="body1">
-                            没有⼴告，不收费⽤，⽀持开源，兼具丰富功能和⾃定设计
+                            {t('banner_intro')}
                         </Typography>
                         <Box className={'flex mb-4 ' + classes.downloadWrapper}>
                             <div className={classNames([classes.fDroid, classes.download])} />
@@ -362,59 +373,57 @@ function HomePage({ t }: { t: TFunction }) {
                 </main>
             )}
             <Typography variant="body1" className="text-gray-400 pt-16">
-                Our Features
+                {t('feature_title')}
             </Typography>
             <Typography variant={xsMatched ? 'h5' : 'h4'} className="text-gray-800 pt-4 pb-16">
-                Great Features Of Twidere X
+                {t('feature_intro')}
             </Typography>
-            <section className="flex justify-center items-center flex-wrap">
-                <ImgSvg_1
+            <section className={'flex justify-around items-center flex-wrap ' + classes.featureWrapper}>
+                <ImgFeatureOne
                     className="max-w-full"
-                    width={screenWidth > 722 ? 722 : screenWidth}
-                    height={screenWidth > 722 ? 722 : (720 * screenWidth) / 722}
+                    width={screenWidth > 640 ? 640 : screenWidth}
+                    height={screenWidth > 640 ? 732 : (732 * screenWidth) / 640}
                 />
                 <Box className={classes.featureBox}>
                     <Typography
                         variant={xsMatched ? 'h5' : 'h4'}
                         className={'text-gray-800 pt-4 ' + classes.featureTitle}>
-                        可定制的主界⾯
+                        {t('feature_one_title')}
                     </Typography>
                     <Typography variant="body1" className={'text-gray-800 pt-4 ' + classes.featureText}>
-                        Twidere X 主界⾯的标签⻚可以根据需要进⾏定制，将不必要的标签⻚去除，让界⾯适合⾃⼰并保持简洁。
+                        {t('feature_one_intro')}
                     </Typography>
                 </Box>
             </section>
-            <section className="flex justify-center items-center flex-wrap-reverse">
+            <section className={'flex flex-wrap-reverse justify-around items-center ' + classes.featureWrapper}>
                 <Box className={classes.featureBox}>
                     <Typography
                         variant={xsMatched ? 'h5' : 'h4'}
                         className={'text-gray-800 pt-4 ' + classes.featureTitle}>
-                        同⼀时间线上查看多帐号消息
+                        {t('feature_two_title')}
                     </Typography>
                     <Typography variant="body1" className={'text-gray-800 pt-4 ' + classes.featureText}>
-                        Twidere X 完美⽀持多帐号切换，Twidere X 也⽀持在同⼀时间线
-                        上不切换帐号就能查看多个帐号时间轴消息，省去繁琐的账户切换。
+                        {t('feature_two_intro')}
                     </Typography>
                 </Box>
-                <ImgSvg_2
-                    width={screenWidth > 698 ? 698 : screenWidth}
-                    height={screenWidth > 698 ? 698 : (608 * screenWidth) / 698}
+                <ImgFeatureTwo
+                    width={screenWidth > 610 ? 610 : screenWidth}
+                    height={screenWidth > 610 ? 672 : (672 * screenWidth) / 610}
                 />
             </section>
-            <section className="flex justify-center items-center flex-wrap">
-                <ImgSvg_1
-                    width={screenWidth > 722 ? 722 : screenWidth}
-                    height={screenWidth > 722 ? 720 : (720 * screenWidth) / 722}
+            <section className={'flex justify-around items-center flex-wrap ' + classes.featureWrapper}>
+                <ImgFeatureThree
+                    width={screenWidth > 610 ? 610 : screenWidth}
+                    height={screenWidth > 610 ? 647 : (647 * screenWidth) / 610}
                 />
                 <Box className={classes.featureBox}>
                     <Typography
                         variant={xsMatched ? 'h5' : 'h4'}
                         className={'text-gray-800 pt-4 ' + classes.featureTitle}>
-                        ⾼度⼈性化的⾃定义设置
+                        {t('feature_three_title')}
                     </Typography>
                     <Typography variant="body1" className={'text-gray-800 pt-4 ' + classes.featureText}>
-                        除了字体以及图片的自由设置，Twidere X
-                        还能自定义按钮摆放，主题颜色及发送方式（例如：回车发送消息）
+                        {t('feature_three_intro')}
                     </Typography>
                 </Box>
             </section>
@@ -429,27 +438,27 @@ function HomePage({ t }: { t: TFunction }) {
                     <Typography
                         variant="body1"
                         className={`text-gray-800 pt-6 ${xsMatched ? 'pl-6' : 'pl-10'} ` + classes.futureIntro}>
-                        更多功能还在开发，
+                        {t('more_under_dev')}
                     </Typography>
                     <Typography
                         variant="body1"
                         className={`text-gray-800 ${xsMatched ? 'pl-6' : 'pl-10'} ` + classes.futureIntro}>
-                        访问我们的Blog查看我们的计划和很棒的内容！
+                        {t('more_within_blog')}
                     </Typography>
                     {xsMatched ? null : (
                         <Button className={classes.futureButton} variant="contained">
-                            View Blog
+                            {t('link_view_blog')}
                         </Button>
                     )}
                 </Box>
                 <Box className="max-w-full flex flex-col items-center">
-                    <ImgSvg_3
+                    <ImgFeatureFour
                         className="max-w-full"
                         height={screenWidth > 407 * 0.8 ? 356 * 0.8 : (356 * screenWidth) / (407 * 0.8)}
                     />
                     {xsMatched ? (
                         <Button className={classes.futureButton} variant="contained">
-                            View Blog
+                            {t('link_view_blog')}
                         </Button>
                     ) : null}
                 </Box>
@@ -457,7 +466,7 @@ function HomePage({ t }: { t: TFunction }) {
             <section className={classes.contactWrapper}>
                 <Box className={'mr-12 ' + classes.contactBox}>
                     <Typography variant="body1" className={'text-gray-400 ' + classes.contactText}>
-                        联系我们
+                        {t('contact_us')}
                     </Typography>
                     <div className="flex justify-between mt-6">
                         <MailIcon className="cursor-pointer" />
@@ -467,7 +476,7 @@ function HomePage({ t }: { t: TFunction }) {
                 </Box>
                 <Box className={classes.contactBox}>
                     <Typography variant="body1" className={'text-gray-400 ' + classes.contactText}>
-                        赞助商
+                        {t('sponsor')}
                     </Typography>
                     <SujitechIcon className="mt-6" />
                 </Box>
@@ -479,7 +488,7 @@ function HomePage({ t }: { t: TFunction }) {
                 }>
                 <Button variant="text" className="mr-16" onClick={(e) => setAnchorEl(e.currentTarget)}>
                     <Box className="flex items-baseline">
-                        <span>简体中文</span>
+                        <span>{t('language')}</span>
                         <ArrowDropDownIcon className="ml-1" />
                     </Box>
                 </Button>
@@ -489,11 +498,25 @@ function HomePage({ t }: { t: TFunction }) {
                     keepMounted
                     open={Boolean(anchorEl)}
                     onClose={() => setAnchorEl(null)}>
-                    <MenuItem onClick={() => setAnchorEl(null)}>简体中文</MenuItem>
-                    <MenuItem onClick={() => setAnchorEl(null)}>English</MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            i18n.changeLanguage('zh')
+                            window.localStorage.setItem('twidere_language', 'zh')
+                            setAnchorEl(null)
+                        }}>
+                        {t('language_chinese')}
+                    </MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            i18n.changeLanguage('en')
+                            window.localStorage.setItem('twidere_language', 'en')
+                            setAnchorEl(null)
+                        }}>
+                        {t('language_english')}
+                    </MenuItem>
                 </Menu>
                 <Typography variant="body1" className={'text-gray-400 pt-2 pl-2 '}>
-                    Twidere Website is maintained by DimensionDev.
+                    {t('maintained_by')}
                 </Typography>
             </footer>
         </Box>
